6a8707d8bf163cc780bb812c705e06d84ff04a94
Michael Ahlers <michael@ahlers.consulting>
2024-01-21 14:32:11 -0700

    Define `Ramp` and add guards

--- src/main/scala/zwift/schema/desktop/WorkoutStep.scala
+++ src/main/scala/zwift/schema/desktop/WorkoutStep.scala
@@ -7,15 +7,53 @@ sealed trait WorkoutStep
 object WorkoutStep {
 
   case class SteadyState(
-    duration: Int,
-    power: Float,
-  ) extends WorkoutStep
+    durationSeconds: Int,
+    ftpPowerRatio: Float,
+  ) extends WorkoutStep {
+
+    require(
+      durationSeconds >= 0,
+      s"Duration ($durationSeconds) must be non-negative.",
+    )
+
+    require(
+      ftpPowerRatio >= 0,
+      s"FTP power ratio ($ftpPowerRatio) must be non-negative.",
+    )
+
+  }
 
   object SteadyState {
     implicit val xmlEncoder: XmlEncoder[SteadyState] = step =>
       <SteadyState
-        Duration={step.duration.toString}
-        Power={step.power.toString} />
+        Duration={step.durationSeconds.toString}
+        Power={step.ftpPowerRatio.toString} />
+  }
+
+  case class Ramp(
+    durationSeconds: Int,
+    ftpPowerLowRatio: Float,
+    ftpPowerHighRatio: Float,
+  ) extends WorkoutStep {
+
+    require(
+      ftpPowerLowRatio >= 0,
+      s"FTP power low ratio ($ftpPowerLowRatio) must be non-negative.",
+    )
+
+    require(
+      ftpPowerLowRatio <= ftpPowerHighRatio,
+      s"FTP power low ratio ($ftpPowerLowRatio) must be less than or equal to FTP power high ratio ($ftpPowerHighRatio).",
+    )
+
+  }
+
+  object Ramp {
+    implicit val xmlEncoder: XmlEncoder[Ramp] = step =>
+      <Ramp
+        Duration={step.durationSeconds.toString}
+        PowerLow={step.ftpPowerLowRatio.toString}
+        PowerHigh={step.ftpPowerHighRatio.toString} />
   }
 
   implicit val xmlEncoder: XmlEncoder[WorkoutStep] = {
