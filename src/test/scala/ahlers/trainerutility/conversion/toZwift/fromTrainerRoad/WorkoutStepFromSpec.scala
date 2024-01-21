03d67d61e8bf0e6172edfd85fae51ca21324058d
Michael Ahlers <michael@ahlers.consulting>
2024-01-21 14:52:00 -0700

    Essential tests

--- src/test/scala/ahlers/trainerutility/conversion/toZwift/fromTrainerRoad/WorkoutStepFromSpec.scala
+++ src/test/scala/ahlers/trainerutility/conversion/toZwift/fromTrainerRoad/WorkoutStepFromSpec.scala
@@ -7,6 +7,7 @@ import com.softwaremill.diffx.scalatest.DiffShouldMatcher._
 import diffx.instances._
 import org.scalatest.wordspec.AnyWordSpec
 import trainerroad.schema.web.IntervalData
+import trainerroad.schema.web.WorkoutData
 import trainerroad.schema.web.diffx.instances._
 import zwift.schema.desktop.diffx.instances._
 
@@ -23,7 +24,29 @@ class WorkoutStepFromSpec extends AnyWordSpec {
       ).shouldMatchTo(Invalid(NoWorkoutsForInterval(
         interval = interval,
       )))
+    }
 
+    "interval ends before workouts" in {
+      val interval: IntervalData = IntervalData(
+        name = null,
+        start = 0,
+        end = 100,
+        isFake = false,
+        startTargetPowerPercent = 0,
+      )
+
+      WorkoutStepFrom(
+        interval = interval,
+        workouts = Seq(
+          WorkoutData(
+            centiseconds = interval.end * 100 + 1,
+            memberFtpPercent = 0,
+            ftpPercent = 0,
+          ),
+        ),
+      ).shouldMatchTo(Invalid(NoWorkoutsForInterval(
+        interval = interval,
+      )))
     }
 
   }
