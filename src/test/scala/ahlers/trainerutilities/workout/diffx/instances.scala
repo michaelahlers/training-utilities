867ca5a0cbdf3369c17c283b8397ea0768ae56a0
Michael Ahlers <michael@ahlers.consulting>
2024-01-21 18:15:20 -0700

    An intermediate representation may not be useful in all cases

--- src/test/scala/ahlers/trainerutilities/workout/diffx/instances.scala
+++ src/test/scala/ahlers/trainerutilities/workout/diffx/instances.scala
@@ -1,14 +1,10 @@
 package ahlers.trainerutilities.workout.diffx
 
 import ahlers.trainerutilities.workout.Error
-import ahlers.trainerutilities.workout.IntervalDetails
-import cats.data.diffx.instances._
 import com.softwaremill.diffx.Diff
 import com.softwaremill.diffx.generic.auto._
-import trainerroad.schema.web.diffx.instances._
 
 object instances {
 
   implicit val diffError: Diff[Error] = Diff.summon
-  implicit val diffIntervalDetails: Diff[IntervalDetails] = Diff.summon
 }
