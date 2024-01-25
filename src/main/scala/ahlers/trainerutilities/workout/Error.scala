346323457e212e0123b54ab524157cbef81b25e0
Michael Ahlers <michael@ahlers.consulting>
2024-01-24 19:41:44 -0700

    Clean up

--- src/main/scala/ahlers/trainerutilities/workout/Error.scala
+++
@@ -1,8 +0,0 @@
-package ahlers.trainerutilities.workout
-
-import trainerroad.schema.web.IntervalData
-
-sealed trait Error
-object Error {
-  case class NoDetailsForInterval(interval: IntervalData) extends Error
-}
