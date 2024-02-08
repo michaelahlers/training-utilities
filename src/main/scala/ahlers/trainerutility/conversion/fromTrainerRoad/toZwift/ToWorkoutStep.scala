93cc6392e81b18bee5fbd7b0cfd84d07a5c14094
Michael Ahlers <michael@ahlers.consulting>
2024-02-08 15:53:16 -0700

    WIP revision

--- src/main/scala/ahlers/trainerutility/conversion/fromTrainerRoad/toZwift/ToWorkoutStep.scala
+++ src/main/scala/ahlers/trainerutility/conversion/fromTrainerRoad/toZwift/ToWorkoutStep.scala
@@ -2,7 +2,9 @@ package ahlers.trainerutility.conversion.fromTrainerRoad.toZwift
 
 import Error.NoWorkoutsForInterval
 import cats.data.Validated
+import cats.data.Validated.Valid
 import cats.syntax.validated._
+import scala.annotation.tailrec
 import trainerroad.schema.web.IntervalData
 import trainerroad.schema.web.Workout
 import trainerroad.schema.web.WorkoutData
@@ -13,13 +15,25 @@ import zwift.schema.desktop.WorkoutStep.Ramp
 import zwift.schema.desktop.WorkoutStep.SteadyState
 import zwift.schema.desktop.WorkoutStep.Warmup
 
-private[toZwift] object ToWorkoutStep {
+/*private[toZwift]*/
+object ToWorkoutStep {
 
   sealed trait Slope
   object Slope {
+    case object Undefined extends Slope
     case object Up extends Slope
     case object Flat extends Slope
     case object Down extends Slope
+
+    def from(start: WorkoutData, end: WorkoutData): Slope =
+      if (start.ftpPercent == end.ftpPercent) Flat
+      else if (start.ftpPercent < end.ftpPercent) Up
+      else Down
+
+    def from(workouts: Seq[WorkoutData]): Slope =
+      if (workouts.size < 2) Undefined
+      else from(workouts.head, workouts.last)
+
   }
 
   sealed trait Phase
@@ -140,6 +154,139 @@ private[toZwift] object ToWorkoutStep {
         (step, next).valid
     }
 
+  }
+
+  def foo(
+    workouts: Seq[WorkoutData],
+  ): Validated[Error, (WorkoutStep, Seq[WorkoutData])] = {
+
+    @tailrec
+    def take(queue: List[WorkoutData], acc: Vector[WorkoutData]): ((WorkoutData, WorkoutData), Seq[WorkoutData]) = {
+      val slope = Slope.from(acc)
+
+      queue match {
+
+        case _ :: Nil =>
+          ((acc.head, acc.last), queue)
+
+        case head :: tail if slope == Slope.Undefined =>
+          take(
+            queue = tail,
+            acc = acc :+ head,
+          )
+
+        case head :: tail if slope == Slope.from(acc.last, head) =>
+          take(
+            queue = tail,
+            acc = acc :+ head,
+          )
+
+        case head :: _ if slope != Slope.from(acc.last, head) =>
+          val nextSlope = Slope.from(acc.last, head)
+          pprint.log((slope, nextSlope))
+          ((acc.head, acc.last), queue)
+
+        case head :: _ if acc.last.ftpPercent != head.ftpPercent =>
+          ((acc.head, acc.last), queue)
+
+        case head :: tail =>
+          take(
+            queue = tail,
+            acc = acc :+ head,
+          )
+
+      }
+    }
+
+    val ((start, end), next) = take(
+      queue = workouts.toList,
+      acc = Vector.empty,
+    )
+
+    /** [[WorkoutFile.workout]] is order dependent, and only the duration is required. */
+    val durationSeconds = (end.milliseconds - start.milliseconds) / 1000
+
+    val phase: Phase =
+      if (start.milliseconds == 0) Phase.First
+      else if (next.size == 1) Phase.Last
+      else Phase.Interior
+
+    val slope: Slope = Slope.from(start, end)
+
+    val step: WorkoutStep = (phase, slope) match {
+
+      case (Phase.First, Slope.Up | Slope.Down) =>
+        val ftpPowerLowPercent = start.ftpPercent
+        val ftpPowerHighPercent = end.ftpPercent
+
+        val ftpPowerLowRatio = ftpPowerLowPercent / 100f
+        val ftpPowerHighRatio = ftpPowerHighPercent / 100f
+
+        Warmup(
+          durationSeconds = durationSeconds,
+          ftpPowerLowRatio = ftpPowerLowRatio,
+          ftpPowerHighRatio = ftpPowerHighRatio,
+        )
+
+      case (Phase.First, Slope.Flat) =>
+        val ftpPowerPercent = start.ftpPercent
+        val ftpPowerRatio = ftpPowerPercent / 100f
+
+        Warmup(
+          durationSeconds = durationSeconds,
+          ftpPowerLowRatio = ftpPowerRatio,
+          ftpPowerHighRatio = ftpPowerRatio,
+        )
+
+      case (Phase.Interior, Slope.Up | Slope.Down) =>
+        val ftpPowerLowPercent = start.ftpPercent
+        val ftpPowerHighPercent = end.ftpPercent
+
+        val ftpPowerLowRatio = ftpPowerLowPercent / 100f
+        val ftpPowerHighRatio = ftpPowerHighPercent / 100f
+
+        Ramp(
+          durationSeconds = durationSeconds,
+          ftpPowerLowRatio = ftpPowerLowRatio,
+          ftpPowerHighRatio = ftpPowerHighRatio,
+        )
+
+      case (Phase.Interior, Slope.Flat) =>
+        val ftpPowerPercent = start.ftpPercent
+        val ftpPowerRatio = ftpPowerPercent / 100f
+
+        SteadyState(
+          durationSeconds = durationSeconds,
+          ftpPowerRatio = ftpPowerRatio,
+        )
+
+      case (Phase.Last, Slope.Up | Slope.Down) =>
+        val ftpPowerLowPercent = start.ftpPercent
+        val ftpPowerHighPercent = end.ftpPercent
+
+        val ftpPowerLowRatio = ftpPowerLowPercent / 100f
+        val ftpPowerHighRatio = ftpPowerHighPercent / 100f
+
+        Cooldown(
+          durationSeconds = durationSeconds,
+          ftpPowerLowRatio = ftpPowerLowRatio,
+          ftpPowerHighRatio = ftpPowerHighRatio,
+        )
+
+      case (Phase.Last, Slope.Flat) =>
+        val ftpPowerPercent = start.ftpPercent
+        val ftpPowerRatio = ftpPowerPercent / 100f
+
+        Cooldown(
+          durationSeconds = durationSeconds,
+          ftpPowerLowRatio = ftpPowerRatio,
+          ftpPowerHighRatio = ftpPowerRatio,
+        )
+
+    }
+
+    if (next.size == 1) (step, Nil).valid
+    else (step, next).valid
   }
 
 }
--- src/main/scala/ahlers/trainerutility/conversion/fromTrainerRoad/toZwift/ToWorkoutSteps.scala
+++ src/main/scala/ahlers/trainerutility/conversion/fromTrainerRoad/toZwift/ToWorkoutSteps.scala
@@ -4,6 +4,7 @@ import cats.data.Validated
 import cats.data.Validated.Invalid
 import cats.data.Validated.Valid
 import cats.syntax.validated._
+import scala.annotation.tailrec
 import trainerroad.schema.web.IntervalData
 import trainerroad.schema.web.WorkoutData
 import zwift.schema.desktop.WorkoutStep
@@ -32,5 +33,29 @@ private[toZwift] object ToWorkoutSteps {
       .map { case (steps, _) =>
         steps
       }
+
+  def foo(
+    workouts: Seq[WorkoutData],
+  ): Validated[Error, Seq[WorkoutStep]] = {
+
+    @tailrec
+    def take(
+      workouts: List[WorkoutData],
+      steps: Vector[WorkoutStep],
+    ): Seq[WorkoutStep] = workouts match {
+      case Nil => steps
+      case workouts =>
+        val (step, next) = ToWorkoutStep.foo(workouts).getOrElse(???)
+        take(
+          workouts = next.toList,
+          steps = steps :+ step,
+        )
+    }
+
+    take(
+      workouts = workouts.toList,
+      steps = Vector.empty,
+    ).valid
+  }
 
 }
