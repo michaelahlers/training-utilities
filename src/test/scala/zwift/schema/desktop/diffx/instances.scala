dd948ae9671061619bcd6b16a9e2acb78348dc73
Michael Ahlers <michael@ahlers.consulting>
2024-02-10 12:30:58 -0700

    Quick fix for tolerance

--- src/test/scala/zwift/schema/desktop/diffx/instances.scala
+++ src/test/scala/zwift/schema/desktop/diffx/instances.scala
@@ -2,14 +2,24 @@ package zwift.schema.desktop.diffx
 
 import com.softwaremill.diffx.Diff
 import com.softwaremill.diffx.generic.auto._
+import zwift.schema.desktop
 import zwift.schema.desktop.WorkoutStep
+import zwift.schema.desktop.WorkoutStep.Cooldown
 import zwift.schema.desktop.WorkoutStep.Ramp
 import zwift.schema.desktop.WorkoutStep.SteadyState
+import zwift.schema.desktop.WorkoutStep.Warmup
 
 object instances {
 
-  implicit val diffSteadyState: Diff[SteadyState] = Diff.summon
-  implicit val diffRamp: Diff[Ramp] = Diff.summon
-  implicit val diffWorkoutStep: Diff[WorkoutStep] = Diff.summon
+  implicit val diffWorkoutStepWarmup: Diff[WorkoutStep.Warmup] = Diff.summon
+  implicit val diffWorkoutStepSteadyState: Diff[WorkoutStep.SteadyState] = Diff.summon
+  implicit val diffWorkoutStepRamp: Diff[WorkoutStep.Ramp] = Diff.summon
+  implicit val diffWorkoutStepCooldown: Diff[WorkoutStep.Cooldown] = Diff.summon
+
+  implicit val diffWorkoutStep: Diff[WorkoutStep] = Diff.summon[WorkoutStep]
+    .modify(_.ftpRatioStart)
+    .setTo(Diff.approximate(0.01f))
+    .modify(_.ftpRatioEnd)
+    .setTo(Diff.approximate(0.01f))
 
 }
