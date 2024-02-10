4cb08bf1fff6575d5275241f7cc0c41f25fa6107
Michael Ahlers <michael@ahlers.consulting>
2024-02-10 09:25:41 -0700

    Arbitrary NEL

--- src/test/scala/cats/data/scalacheck/instances.scala
+++ src/test/scala/cats/data/scalacheck/instances.scala
@@ -1,6 +1,7 @@
 package cats.data.scalacheck
 
 import cats.data.NonEmptyChain
+import cats.data.NonEmptyList
 import org.scalacheck.Arbitrary
 import org.scalacheck.Arbitrary.arbitrary
 
@@ -9,9 +10,17 @@ object instances {
   implicit def arbNonEmptyChain[A: Arbitrary]: Arbitrary[NonEmptyChain[A]] = Arbitrary(for {
     head <- arbitrary[A]
     tail <- arbitrary[Seq[A]]
-  } yield NonEmptyChain.of(
+  } yield NonEmptyChain(
+    a = head,
+    as = tail: _*,
+  ))
+
+  implicit def arbNonEmptyList[A: Arbitrary]: Arbitrary[NonEmptyList[A]] = Arbitrary(for {
+    head <- arbitrary[A]
+    tail <- arbitrary[List[A]]
+  } yield NonEmptyList(
     head = head,
-    tail = tail: _*,
+    tail = tail,
   ))
 
 }
