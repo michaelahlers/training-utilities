2accec6c4dcf6bfeec891480aee040d62e21cf2f
Michael Ahlers <michael@ahlers.consulting>
2024-02-10 08:55:42 -0700

    Clean up generators

--- src/test/scala/zwift/schema/desktop/scalacheck/instances.scala
+++ src/test/scala/zwift/schema/desktop/scalacheck/instances.scala
@@ -4,13 +4,47 @@ import magnolify.scalacheck.semiauto._
 import org.scalacheck.Arbitrary
 import org.scalacheck.Gen
 import zwift.schema.desktop.WorkoutStep
+import zwift.schema.desktop.WorkoutStep.Cooldown
+import zwift.schema.desktop.WorkoutStep.Ramp
+import zwift.schema.desktop.WorkoutStep.SteadyState
+import zwift.schema.desktop.WorkoutStep.Warmup
 
 object instances {
 
-  implicit val arbWorkoutStep: Arbitrary[WorkoutStep] = {
-    implicit val arbInt: Arbitrary[Int] = Arbitrary(Gen.choose(5 * 60, 20 * 60))
-    implicit val arbFloat: Arbitrary[Float] = Arbitrary(Gen.choose(40f, 120f))
+  private val genPartialWorkoutStep: Gen[(Int, Float, Float)] =
+    for {
+      durationSeconds <- Gen.posNum[Int].map(_ + 1)
+      ftpRatioStart <- Gen.posNum[Float]
+      ftpRatioEnd <- Gen.posNum[Float]
+    } yield (durationSeconds, ftpRatioStart, ftpRatioEnd)
+
+  val genWarmup: Gen[Warmup] =
+    genPartialWorkoutStep.map((Warmup.apply _).tupled)
+
+  implicit val arbWarmup: Arbitrary[Warmup] =
+    Arbitrary(genWarmup)
+
+  val genSteadyState: Gen[SteadyState] =
+    genPartialWorkoutStep.map { case (durationSeconds, ftpRatioStart, _) =>
+      SteadyState(durationSeconds, ftpRatioStart)
+    }
+
+  implicit val arbSteadyState: Arbitrary[SteadyState] =
+    Arbitrary(genSteadyState)
+
+  val genRamp: Gen[Ramp] =
+    genPartialWorkoutStep.map((Ramp.apply _).tupled)
+
+  implicit val arbRamp: Arbitrary[Ramp] =
+    Arbitrary(genRamp)
+
+  val genCooldown: Gen[Cooldown] =
+    genPartialWorkoutStep.map((Cooldown.apply _).tupled)
+
+  implicit val arbCooldown: Arbitrary[Cooldown] =
+    Arbitrary(genCooldown)
+
+  implicit val arbWorkoutStep: Arbitrary[WorkoutStep] =
     ArbitraryDerivation[WorkoutStep]
-  }
 
 }
