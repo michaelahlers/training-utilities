0da3bf140b3ada6eefa20ff7d4c1d001b46a03ff
Michael Ahlers <michael@ahlers.consulting>
2024-02-11 11:56:37 -0700

    Finish collection support

--- src/main/scala/scala/xml/encoding/instances.scala
+++ src/main/scala/scala/xml/encoding/instances.scala
@@ -1,11 +1,15 @@
 package scala.xml.encoding
 
-//import scala.xml.NodeSeq
-//import scala.xml.encoding.syntax._
+import cats.data.NonEmptyList
+import scala.xml.NodeSeq
+import scala.xml.encoding.syntax._
 
 object instances {
 
-  // implicit def xmlEncoderSeq[A: XmlEncoder]: XmlEncoder[Seq[A]] = as =>
-  //  NodeSeq.fromSeq(as.map(_.asXml))
+  implicit def xmlEncoderSeq[C[X] <: Iterable[X], A: XmlEncoder]: XmlEncoder[C[A]] = as =>
+    NodeSeq.fromSeq(as.flatMap(_.asXml).toSeq)
+
+  implicit def xmlEncoderNonEmptyList[A: XmlEncoder]: XmlEncoder[NonEmptyList[A]] = as =>
+    as.toList.asXml
 
 }
