87cfe1753b07ff07564634b09e306c13f4938b58
Michael Ahlers <michael@ahlers.consulting>
2024-02-11 11:24:44 -0700

    Revise errors

--- src/main/scala/ahlers/trainerutility/conversion/fromTrainerRoad/toZwift/Error.scala
+++ src/main/scala/ahlers/trainerutility/conversion/fromTrainerRoad/toZwift/Error.scala
@@ -1,9 +1,9 @@
 package ahlers.trainerutility.conversion.fromTrainerRoad.toZwift
 
+import cats.data.NonEmptyList
 import trainerroad.schema.web.WorkoutData
 
 sealed trait Error
 object Error {
-  case object NoWorkoutsForStep extends Error
-  case class UndefinedSlope(workouts: Seq[WorkoutData]) extends Error
+  case class NoIntervalsInWorkout(workouts: NonEmptyList[WorkoutData]) extends Error
 }
