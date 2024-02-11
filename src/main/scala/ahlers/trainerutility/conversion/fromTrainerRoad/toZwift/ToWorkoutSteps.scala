d5298ee007e4ba31285a26032d4e620ba9508e39
Michael Ahlers <michael@ahlers.consulting>
2024-02-11 08:04:10 -0700

    Extract `Slope`

--- src/main/scala/ahlers/trainerutility/conversion/fromTrainerRoad/toZwift/ToWorkoutSteps.scala
+++ src/main/scala/ahlers/trainerutility/conversion/fromTrainerRoad/toZwift/ToWorkoutSteps.scala
@@ -10,6 +10,7 @@ import org.scalactic.Tolerance._
 import org.scalactic.TripleEquals._
 import scala.annotation.tailrec
 import trainerroad.schema.web.WorkoutData
+import trainerroad.view.Workout.Slope
 import zwift.schema.desktop.WorkoutFile
 import zwift.schema.desktop.WorkoutStep
 import zwift.schema.desktop.WorkoutStep.Cooldown
@@ -18,39 +19,6 @@ import zwift.schema.desktop.WorkoutStep.SteadyState
 import zwift.schema.desktop.WorkoutStep.Warmup
 
 private[toZwift] object ToWorkoutSteps {
-
-  sealed trait Slope
-  object Slope {
-
-    case object Undefined extends Slope
-    sealed trait Defined {
-      def ratio: Float
-    }
-
-    case object Zero extends Slope with Defined {
-      override val ratio: Float = 0
-    }
-
-    case class Positive(ratio: Float) extends Slope with Defined
-
-    case class Negative(ratio: Float) extends Slope with Defined
-
-    def apply(value: Float): Slope with Defined =
-      if (value > 0) Positive(value)
-      else if (value < 0) Negative(value)
-      else Zero
-
-    def from(start: WorkoutData, end: WorkoutData): Slope with Defined =
-      Slope {
-        (end.ftpPercent - start.ftpPercent) /
-          (end.milliseconds - start.milliseconds)
-      }
-
-    def from(workouts: Seq[WorkoutData]): Slope =
-      if (workouts.size > 1) from(workouts.head, workouts.last)
-      else Undefined
-
-  }
 
   sealed trait Phase
   object Phase {
