6fc383665062e184c76ac61d9f87237b5471f935
Michael Ahlers <michael@ahlers.consulting>
2024-02-21 18:14:47 -0700

    Configure default logging

--- modules/tools/src/main/scala/ahlers/training/tools/ToolsCliApp.scala
+++ modules/tools/src/main/scala/ahlers/training/tools/ToolsCliApp.scala
@@ -2,14 +2,16 @@ package ahlers.training.tools
 
 import ahlers.training.tools.conversion.ConversionToolsTask
 import ahlers.trainingutilities.tools.BuildInfo
-import java.net.URI
-import scala.util.control.NonFatal
 import zio.Console.printLine
 import zio._
 import zio.cli.HelpDoc.Span.text
 import zio.cli._
+import zio.logging.consoleLogger
 
 object ToolsCliApp extends ZIOCliDefault {
+
+  override val bootstrap =
+    Runtime.removeDefaultLoggers >>> consoleLogger()
 
   val helpDoc: HelpDoc = HelpDoc.p("Converts given workout into a different format.")
 
@@ -23,9 +25,7 @@ object ToolsCliApp extends ZIOCliDefault {
     summary = text("Tools for working with training data."),
     command = command,
   ) { command =>
-    // Implement logic of CliApp
     ZIO.logInfo(s"$command")
-    printLine("executing git clone")
   }
 
 }
