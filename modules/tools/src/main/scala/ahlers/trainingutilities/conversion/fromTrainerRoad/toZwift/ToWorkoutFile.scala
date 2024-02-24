940bdf0be3078df8c98627b9e8f6991a0669e555
Michael Ahlers <michael@ahlers.consulting>
2024-02-24 14:27:34 -0700

    Sanitize name and description

--- modules/tools/src/main/scala/ahlers/trainingutilities/conversion/fromTrainerRoad/toZwift/ToWorkoutFile.scala
+++ modules/tools/src/main/scala/ahlers/trainingutilities/conversion/fromTrainerRoad/toZwift/ToWorkoutFile.scala
@@ -1,23 +1,53 @@
 package ahlers.trainingutilities.conversion.fromTrainerRoad.toZwift
 
+import cats.data.NonEmptyList
 import cats.data.Validated
+import cats.implicits._
+import org.jsoup.Jsoup
+import scala.util.control.NoStackTrace
 import trainerroad.schema.web.Workout
 import trainerroad.schema.web.WorkoutDetails
 import zwift.schema.desktop.WorkoutFile
+import zwift.schema.desktop.WorkoutStep
 
 object ToWorkoutFile {
 
-  def from(workout: Workout): Validated[Throwable, WorkoutFile] =
-    for {
-      steps <- ToWorkoutSteps.from(workout.workoutData)
-    } yield WorkoutFile(
-      author = "TrainerRoad",
-      name = workout.details.workoutName,
-      description = workout.details.workoutDescription,
-      sportType = "bike",
-      tags = workout.tags.map(WorkoutFile.Tag(_)),
-      workout = steps,
-    )
+  /** @todo Make this a whole lot more simple. */
+  def from(workout: Workout): Validated[Throwable, WorkoutFile] = {
+
+    val stepsF: Validated[NonEmptyList[Throwable], NonEmptyList[WorkoutStep]] =
+      ToWorkoutSteps.from(workout.workoutData)
+        .toValidatedNel
+
+    val nameF: Validated[NonEmptyList[Throwable], String] = Validated.catchNonFatal(Jsoup
+      .parse(workout.details.workoutName)
+      .root()
+      .text())
+      .toValidatedNel
+
+    val descriptionF: Validated[NonEmptyList[Throwable], String] = Validated.catchNonFatal(Jsoup
+      .parse(workout.details.workoutDescription)
+      .root()
+      .text())
+      .toValidatedNel
+
+    (stepsF, nameF, descriptionF)
+      .mapN((steps, name, description) =>
+        WorkoutFile(
+          author = "TrainerRoad",
+          name = name,
+          description = description,
+          sportType = "bike",
+          tags = workout.tags.map(WorkoutFile.Tag(_)),
+          workout = steps,
+        ),
+      )
+      .leftMap(_
+        .foldLeft(new IllegalArgumentException("Couldn't convert TrainerRoad workout to Zwift workout.") with NoStackTrace) { (error, next) =>
+          error.addSuppressed(next)
+          error
+        })
+  }
 
   def from(workout: WorkoutDetails): Validated[Throwable, WorkoutFile] =
     from(workout.workout)
