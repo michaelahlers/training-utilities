964c320dfc5381e6e6e23370c09f529558d08eef
Michael Ahlers <michael@ahlers.consulting>
2024-02-24 13:57:33 -0700

    Use type members; deal with bytes

--- modules/tools/src/main/scala/ahlers/training/tools/conversion/Conversion.scala
+++ modules/tools/src/main/scala/ahlers/training/tools/conversion/Conversion.scala
@@ -3,43 +3,57 @@ package ahlers.training.tools.conversion
 import scala.xml.encoding.syntax._
 import zio.ZIO
 import zio.json.JsonDecoder
+import zio.json.JsonStreamDelimiter
 import zio.stream.ZPipeline
 
-sealed trait Conversion[A, B] {
+sealed trait Conversion {
 
-  def decode: ZPipeline[Any, Throwable, Char, A]
+  type From
+  type To
 
-  def convert: ZPipeline[Any, Throwable, A, B]
+  def decode: ZPipeline[Any, Throwable, Byte, From]
 
-  def encode: ZPipeline[Any, Throwable, B, Char]
+  def convert: ZPipeline[Any, Throwable, From, To]
+
+  def encode: ZPipeline[Any, Throwable, To, Byte]
+
+  final def total: ZPipeline[Any, Throwable, Byte, Byte] =
+    decode >>>
+      convert >>>
+      encode
 
 }
 
 object Conversion {
 
-  case object FromTrainerRoadWorkoutToZwiftWorkout extends Conversion[TrainerRoadWorkout, ZwiftWorkout] {
+  case object FromTrainerRoadWorkoutToZwiftWorkout extends Conversion {
+
+    override type From = TrainerRoadWorkout
+    override type To   = ZwiftWorkout
 
     import ahlers.trainingutilities.conversion.fromTrainerRoad.toZwift.ToWorkoutFile
     import scala.xml.PrettyPrinter
 
-    override val decode: ZPipeline[Any, Throwable, Char, TrainerRoadWorkout] =
-      JsonDecoder[TrainerRoadWorkout].decodeJsonPipeline()
+    override val decode: ZPipeline[Any, Throwable, Byte, From] =
+      ZPipeline.utf8Decode >>>
+        ZPipeline[String].mapChunks(_.flatMap(_.toCharArray)) >>>
+        JsonDecoder[From].decodeJsonPipeline(JsonStreamDelimiter.Newline)
 
-    override val convert: ZPipeline[Any, Throwable, TrainerRoadWorkout, ZwiftWorkout] =
+    override val convert: ZPipeline[Any, Throwable, From, To] =
       ZPipeline.mapZIO(ToWorkoutFile
         .from(_)
         .map(ZIO.succeed(_))
         .valueOr(ZIO.fail(_)))
 
-    override val encode: ZPipeline[Any, Throwable, ZwiftWorkout, Char] = {
+    override val encode: ZPipeline[Any, Throwable, To, Byte] = {
       val printer = new PrettyPrinter(160, 2)
 
-      ZPipeline[ZwiftWorkout]
+      ZPipeline[To]
         .map(_
           .asXml)
         .map(printer
           .formatNodes(_)
-          .toCharArray.toSeq)
+          .getBytes.toSeq)
         .flattenIterables
     }
   }
