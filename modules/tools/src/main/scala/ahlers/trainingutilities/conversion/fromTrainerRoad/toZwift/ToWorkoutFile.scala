94d4738ba1cc8b8a02e49170ca6cc1f5384990d1
Michael Ahlers <michael@ahlers.consulting>
2024-02-24 11:43:21 -0700

    Restore conversion

---
+++ modules/tools/src/main/scala/ahlers/trainingutilities/conversion/fromTrainerRoad/toZwift/ToWorkoutFile.scala
@@ -0,0 +1,21 @@
+package ahlers.trainingutilities.conversion.fromTrainerRoad.toZwift
+
+import cats.data.Validated
+import trainerroad.schema.web.Workout
+import zwift.schema.desktop.WorkoutFile
+
+object ToWorkoutFile {
+
+  def from(workout: Workout): Validated[Error, WorkoutFile] =
+    for {
+      steps <- ToWorkoutSteps.from(workout.workoutData)
+    } yield WorkoutFile(
+      author = "TrainerRoad",
+      name = workout.details.workoutName,
+      description = workout.details.workoutDescription,
+      sportType = "bike",
+      tags = workout.tags.map(WorkoutFile.Tag(_)),
+      workout = steps,
+    )
+
+}
