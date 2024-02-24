394e801b49365cc2c18f3e143c345ae342b652b7
Michael Ahlers <michael@ahlers.consulting>
2024-02-23 17:46:46 -0700

    Don't need this abstraction (yet)

--- modules/tools/src/main/scala/ahlers/training/tools/ToolsApp.scala
+++ modules/tools/src/main/scala/ahlers/training/tools/ToolsApp.scala
@@ -1,22 +1,35 @@
 package ahlers.training.tools
 
 import ahlers.training.tools.conversion.ConversionApp
-import ahlers.training.tools.conversion.ConversionToolsTask
 import ahlers.trainingutilities.tools.BuildInfo
 import zio._
 import zio.cli.HelpDoc.Span.text
 import zio.cli._
 import zio.logging.consoleLogger
 
-object ToolsCliApp extends ZIOCliDefault {
+object ToolsApp extends ZIOCliDefault {
+
+  sealed trait DryRun
+  object DryRun {
+    case object IsDry extends DryRun
+    case object IsWet extends DryRun
+
+    val options: Options[DryRun] = Options
+      .boolean("dry-run").alias("n")
+      .map {
+        case true  => IsDry
+        case false => IsWet
+      }
+
+  }
 
   override val bootstrap =
     Runtime.removeDefaultLoggers >>> consoleLogger()
 
   val helpDoc: HelpDoc = HelpDoc.p("Converts given workout into a different format.")
 
-  val command: Command[ToolsTask] = Command("tools", Options.none, Args.none)
-    .subcommands(ConversionToolsTask.command)
+  val command: Command[ZIOApp] = Command("tools", Options.none, Args.none)
+    .subcommands(ConversionApp.command)
     .withHelp(helpDoc)
 
   override val cliApp = CliApp.make(
@@ -24,9 +37,12 @@ object ToolsCliApp extends ZIOCliDefault {
     version = BuildInfo.version,
     summary = text("Tools for working with training data."),
     command = command,
-  ) { case command: ConversionToolsTask =>
-    ZIO.logInfo(s"$command")
-    ConversionApp(command).run
+  ) {
+    case app: ConversionApp =>
+      for {
+        _ <- ZIO.logDebug(s"$app")
+        _ <- app.run
+      } yield ()
   }
 
 }
--- modules/tools/src/main/scala/ahlers/training/tools/conversion/ConversionApp.scala
+++ modules/tools/src/main/scala/ahlers/training/tools/conversion/ConversionApp.scala
@@ -1,11 +1,110 @@
 package ahlers.training.tools.conversion
 
+import ahlers.training.tools.ToolsApp
+import java.net.URI
+import scala.util.Try
 import zio._
+import zio.cli.Args
+import zio.cli.Command
+import zio.cli.HelpDoc
+import zio.cli.Options
 
 case class ConversionApp(
-  toolsTask: ConversionToolsTask,
-) extends ZIOAppDefault {
+  dryRun: ToolsApp.DryRun,
+  input: ConversionApp.Input,
+  output: ConversionApp.Output,
+) extends ZIOAppDefault { self =>
+
   val run = for {
-    _ <- ZIO.logInfo(s"ConversionApp created with $toolsTask.")
+    _ <- ZIO.logInfo(s"Bear: $self")
   } yield ()
+
+}
+
+object ConversionApp {
+
+  sealed trait InputFormat
+  object InputFormat {
+
+    case object TrainerRoadWorkout extends InputFormat
+
+    val options: Options[InputFormat] = Options
+      .text("input-format")
+      .map {
+        case "trainer-road-workout" => TrainerRoadWorkout
+      }
+
+  }
+
+  sealed trait OutputFormat
+  object OutputFormat {
+
+    case object ZwiftWorkout extends OutputFormat
+
+    val options: Options[OutputFormat] = Options
+      .text("output-format")
+      .map {
+        case "zwift-workout" => ZwiftWorkout
+      }
+
+  }
+
+  case class InputLocation(toUri: URI)
+  object InputLocation {
+    val args: Args[InputLocation] = Args
+      .text("input-location")
+      .mapOrFail { location =>
+        Try(Right(new URI(location)))
+          .getOrElse(Left(HelpDoc.p(s"""Can't parse input location, "$location", as a URI.""")))
+      }
+      .map(InputLocation(_))
+  }
+
+  case class OutputLocation(toUri: URI)
+  object OutputLocation {
+    val args: Args[OutputLocation] = Args
+      .text("output-location")
+      .mapOrFail { location =>
+        Try(Right(new URI(location)))
+          .getOrElse(Left(HelpDoc.p(s"""Can't parse output location, "$location", as a URI.""")))
+      }
+      .map(OutputLocation(_))
+  }
+
+  case class Input(
+    format: InputFormat,
+    location: InputLocation,
+  )
+
+  case class Output(
+    format: OutputFormat,
+    location: OutputLocation,
+  )
+
+  val options: Options[(ToolsApp.DryRun, InputFormat, OutputFormat)] =
+    ToolsApp.DryRun.options ++
+      InputFormat.options ++
+      OutputFormat.options
+
+  val args: Args[(InputLocation, OutputLocation)] =
+    InputLocation.args ++
+      OutputLocation.args
+
+  val helpDoc: HelpDoc = HelpDoc.p("Converts given workout or activity into a different format.")
+
+  val command: Command[ConversionApp] = Command("convert", options, args).withHelp(helpDoc).map {
+    case ((dryRun, inputFormat, outputFormat), (inputLocation, outputLocation)) =>
+      ConversionApp(
+        dryRun = dryRun,
+        input = Input(
+          format = inputFormat,
+          location = inputLocation,
+        ),
+        output = Output(
+          format = outputFormat,
+          location = outputLocation,
+        ),
+      )
+  }
+
 }
