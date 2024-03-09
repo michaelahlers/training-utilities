246655999d0f8167ffd4c6b214799575859844bf
Michael Ahlers <michael@ahlers.consulting>
2024-03-09 16:45:52 -0700

    Clean up

--- modules/tools/src/test/scala/ahlers/trainingutilities/conversion/fromTrainerRoad/toZwift/diffx/instances.scala
+++ modules/tools/src/test/scala/ahlers/trainingutilities/conversion/fromTrainerRoad/toZwift/diffx/instances.scala
@@ -1,11 +1,8 @@
 package ahlers.trainingutilities.conversion.fromTrainerRoad.toZwift.diffx
 
-import ahlers.trainingutilities.conversion.fromTrainerRoad.toZwift
 import ahlers.trainingutilities.conversion.fromTrainerRoad.toZwift.NoIntervalsInWorkoutException
-import cats.data.diffx.instances._
 import com.softwaremill.diffx.Diff
 import com.softwaremill.diffx.generic.auto._
-import trainerroad.schema.web.diffx.instances._
 
 object instances {
 
